53d1384371325905ee2fc8fd0ceb8fabb9264eb4
Preet Inder Singh <preethazrah@gmail.com>
2021-04-21 21:02:44 +0530

    Removed extra code

--- GenerateTrainingData.py
+++ GenerateTrainingData.py
@@ -10,7 +10,6 @@ def getTrainingSet(csvPath):
 
     trainingRowMask = '["*text*",{"entities":[[*recStart*,*recEnd*,"RECIPIENT"],[*givStart*,*givEnd*,"GIVER"],[*relStart*,*relEnd*,"RELATIONSHIP"],[*streetStart*,*streetEnd*,"STREET"],[*cityStart*,*cityEnd*,"CITY"],[*provStart*,*provEnd*,"PROVINCE"],[*postalStart*,*postalEnd*,"POSTAL"]]}]'
     trainingSet = []
-    trainingRows = ''
     for row in taggedData:
         tempRow = trainingRowMask.replace('*text*',str(row[0])).replace('*recStart*',str(row[2])).replace('*recEnd*',str(row[3])).replace('*givStart*',str(row[5])).replace('*givEnd*',str(row[6])).replace('*relStart*',str(row[8])).replace('*relEnd*',str(row[9])).replace('*streetStart*',str(row[11])).replace('*streetEnd*',str(row[12])).replace('*cityStart*',str(row[14])).replace('*cityEnd*',str(row[15])).replace('*provStart*',str(row[17])).replace('*provEnd*',str(row[18])).replace('*postalStart*',str(row[20])).replace('*postalEnd*',str(row[21]))        
         tempRowJson = json.loads(tempRow)
